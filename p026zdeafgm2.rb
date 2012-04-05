f0cbbd046b4a4d588950367aa2c6410386e90887
Jason Brooks <jason@jgbmusic.com>
2012-04-05 07:04:33 -0500

    add second ternary at line 4

--- p026zdeafgm2.rb
+++ p026zdeafgm2.rb
@@ -3,17 +3,14 @@
 #################
 
 puts "Deary, why don't you say hi to your Grandma?"
-BYE_counter = 0
+bye_counter = 0
 
-while BYE_counter != 3
+until bye_counter == 3
 
   input = gets.chomp
 
   puts (input != input.upcase ?  'HUH?! SPEAK UP, SONNY!' : "NO, NOT SINCE #{rand(1930..1950)}!")
 
-  if input == 'BYE'
-    BYE_counter += 1
-  else
-    BYE_counter = 0
-  end
+  input == 'BYE' ? bye_counter +=1 : bye_counter = 0
+
 end
